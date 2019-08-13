5074a6c144b034a905c021ca29e4ce56b22b108b
priya518 <53899846+priya518@users.noreply.github.com>
2019-08-13 10:58:33 +0530

    Update main.swift

--- operation/main.swift
+++ operation/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+class Employee
+{
+    private var name:String = ""
+    private var sal:Int = 0
+    private var department:String = ""
+    private var designation:String = ""
+
+    public func SetData(name:String,sal:Int,department:String,designation:String)
+    {
+        self.name = name
+        self.sal = sal
+        self.department = department
+        self.designation = designation
+    }
+
+    public func GetData()
+{
+    print("Name : \(self.name)")
+    print("Salary : \(self.sal)")
+    print("Department : \(self.department)")
+    print("Designation : \(self.designation)")
+}
+
+}
+let obj1 = Employee()
+let obj2 = Employee()
+let obj3 = Employee()
+let obj4 = Employee()
+
+
+ obj1.SetData(name:"Priya",sal:10000,department:"IT",designation:"HOD")
+ obj2.SetData(name:"Murli",sal:20000,department:"CE",designation:"Fac")
+ obj3.SetData(name:"Yogesh",sal:30000,department:"ME",designation:"Sub")
+ obj4.SetData(name:"Tanu",sal:40000,department:"BE",designation:"Ass")
+obj1.GetData()
+obj2.GetData()
+obj3.GetData()
+obj4.GetData()
+
 
